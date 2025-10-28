d3aa478c7bc098201552d0833336896416fb17ed
Augusto Neri <augustodeo.neri@gmail.com>
2025-10-28 10:14:53 -0400

    avancando

--- src/components/InputComponent/index.js
+++ src/components/InputComponent/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function InputComponent({ label, type, placeholder }) {
     return (
-        <div style={{display: 'flex'}}>
-            <label>{label}</label>
-            <input type={type} placeholder={placeholder} />
+        <div className='input-group w-50'>
+            <label className="input-group-text">{label}</label>
+            <input className='form-control' type={type} placeholder={placeholder} />
         </div>
     )
 }
--- src/pages/Produto/index.js
+++ src/pages/Produto/index.js
@@ -1,7 +1,35 @@
+import InputComponent from "../../components/InputComponent";
+
 //Produto
 function Produto() {
+  function handleSubmit(evento) {
+    evento.preventDefault();
+    alert('Produto cadastrado com sucesso!');
+  }
+
   return (
-    <h1>Produto</h1>
+    <div>
+      <h1>Produto</h1>
+
+      <div className="bg-light p-3 rounded-3 w-50 m-auto">
+        <form onSubmit={(evento) => handleSubmit(evento)}>
+          <InputComponent
+            label="Nome do Produto"
+            name="nome"
+            type="text" />
+          <InputComponent
+            label="Preço do Produto"
+            name="preco"
+            type="number" />
+          <InputComponent
+            label="Quantidade do Produto"
+            name="quantidade"
+            type="number" />
+          <button className="btn btn-success mt-3"
+            type="submit" >Cadastrar</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
